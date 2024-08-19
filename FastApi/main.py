8147a5225be5304069609325a68d3fac1399ab76
nicolasviruel <nicolasviruel@gmail.com>
2024-08-19 13:26:54 -0300

    Se agrega encriptacion con Bcrypt

--- FastApi/main.py
+++ FastApi/main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 # Importo el Router
 from routers import products, users
 
+# Importo la clase staticFile para los archivos estaticos
+from fastapi.staticfiles import StaticFiles
+
 
 
 
@@ -11,6 +14,11 @@ app = FastAPI()
 # Routers
 app.include_router(products.router) 
 app.include_router(users.router) 
+
+# Montar recursos estaticos ()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 
 # Inicio del servidor uvicorn main:app --reload
 @app.get("/")
--- FastApi/routers/jwt_auth_users.py
+++ FastApi/routers/jwt_auth_users.py
@@ -6,11 +6,22 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 # OAuth2PasswordRequestForm : Mecanismo para capturar el email y la constraseña en este caso
 # OAuth2PasswordBearer: Seria el sistema de Authentication
 
+# Importamos jwt
+from jose import jwt
+# Importamos el Crytp
+from passlib.context import CryptContext
+
+# Algoritmo de Hash
+ALGORITHM = "HS256"
 
 app = FastAPI()
 
 # Criterio de Authentication 
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
+
+# Contexto de encriptacion
+
+crypt = CryptContext(schemes=["bcrypt"])
 
 # Entidad User
 class User(BaseModel):
@@ -40,4 +51,26 @@ users_db = {
         "disabled": True,
         "password": "654321"
     }
-}       
+}
+
+
+# Operacion de consulta para saber si nuestro usuario esta en nuestra base de datos
+def search_user_db(username: str):
+    if username in users_db:
+        return UserDB(**users_db[username]) #realizamos la busqueda en la base de datos con la clave "username", utilizamos los doble ** para que la clase UserDB pueda recibir varios parametros
+
+
+
+
+@app.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):  #lo importante es que sea de tipo Form, para envio de formulario. El Depends 
+# Depends va a depender si el usuario esta autorizado o no para poder hacer las consultas.
+    user_db = users_db.get(form.username)
+    if not user_db:
+        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST, detail= "El usuario no es correcto")
+
+    user = search_user_db(form.username) #buscamos el usuario con la funcion de busqueda que creamos
+    if not form.password == user.password:  #comprobamos si la contraseña que nos llego coincide con el usuario de la base de datos
+        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST, detail= "La contraseña no es correcta")
+
+    return {"access_token": user.username , "token_type": "bearer"} #retornamos el access token si el usuario existe    
